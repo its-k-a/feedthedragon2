921a7988bd3babec18e75ed850312846526c0795
Keyon Ashby <ashbykey000@warren.k12.in.us>
2024-09-18 10:14:13 -0400

    third  commit

--- main.py
+++ main.py
@@ -1,18 +1,111 @@
 import pygame
 
+
 pygame.init()
+
 
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 400
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Feed The Dragon")
+pygame.display.set_caption("Feed the Dragon")
 
+
+FPS = 60
+clock = pygame.time.Clock()
+
+
+
+PLAYER_STARTING_LIVES = 5
+PLAYER_VELOCITY = 10
+COIN_STARTING_VELOCITY = 10
+COIN_ACCELERATION = 0.5
+BUFFER_DISTANCE = 100
+
+
+
+
+score = 0
+player_lives = PLAYER_STARTING_LIVES
+coin_velocity = COIN_STARTING_VELOCITY
+
+
+GREEN = (0, 255, 0)
+DARKGREEN = (10, 50, 10)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+font = pygame.font.Font('AttackGraffiti.ttf', 32)
+
+
+
+score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 10)
+
+
+title_text = font.render("Feed The Dragon", True, GREEN, WHITE)
+title_rect = title_text.get_rect()
+title_rect.centerx = WINDOW_WIDTH // 2
+
+
+lives_text = font.render("Lives:" + str(player_lives), True, GREEN, DARKGREEN)
+lives_rect = lives_text.get_rect()
+lives_rect.topright = (WINDOW_WIDTH - 10, 10)
+
+
+#Set Text for Game Over (Similar to Score)
+'''
+variable names:  game_over_text , game_over_rect 
+render text: "GAMEOVER"
+antialias: True
+color: GREEN
+background: DARKGREEN
+rect location: center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2) 
+'''
+
+
+
+#Set Text for Continue (Similar to Score)
+'''
+variable names:  continue_text, continue_rect  
+render text: "Press any key to play again"
+antialias: True
+color: GREEN
+background: DARKGREEN
+rect location: center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 32)
+'''
+
+coin_sound = pygame.mixer.Sound("coin_sound.wav")
+miss_sound = pygame.mixer.Sound("miss_sound.wav")
+miss_sound.set_volume(0.1)
+pygame.mixer.music.load("ftd_background_music.wav")
+
+player_image = pygame.image.load("dragon_right.png")
+player_rect = player_image.get_rect()
+player_rect.left = 32
+player_rect.centery = WINDOW_HEIGHT //2
+
+coin_image = pygame.image.load("coin.png")
+coin_rect = coin_image.get_rect()
+
+pygame.mixer.music.play(1, 0.0)
+
+
+#Main game loop
 running = True
 while running:
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
-            print("QUIT DETECTED !!!!", "Setting running to False")
-            running= False
+            running = False
+
+    display_surface.fill(BLACK)
+
+    display_surface.blit(score_text, score_rect)
+    display_surface.blit(title_text , title_rect)
+    display_surface.blit(lives_text, lives_rect)
+
+    pygame.display.update()
+    clock.tick(FPS)
 
 pygame.quit()
